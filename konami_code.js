cae988376d3be38ded372b91131867e84b4dd4ea
Nikita Neganov <nikitaneganov@icloud.com>
2017-10-17 07:15:53 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -4,11 +4,11 @@ function init() {
   // Write your JavaScript code inside the init() function
   let nums = [];
   while(nums.includes(38, 38, 40, 40, 37, 39, 37, 39, 66, 65) === false){
-    if (e.which === 32){
-      break
-    }
-    document.addEventListener('keydown', (event) => {
+      document.addEventListener('keydown', (event) => {
       //alert(event.which);
+      if (event.which === 32){
+        break
+      }
       nums.push(event.which);
       alert(nums)
     })
