e035059bb2b8c08f51b6a8604198ee81a2ef57e6
Nikita Neganov <nikitaneganov@icloud.com>
2017-10-17 09:07:52 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,7 +2,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
   // Write your JavaScript code inside the init() function
   let index = 0;
-  document.addEventListener('keydown', onKeyDownHandler);
+  document.body.addEventListener('keydown', onKeyDownHandler);
   function onKeyDownHandler(e){
     const key = parseInt(e.which);
     if (key === code[index]){
