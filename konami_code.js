fff6999f487735b5368a285a4788142db357bda5
Nikita Neganov <nikitaneganov@icloud.com>
2017-10-17 07:11:19 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,12 +3,11 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
   // Write your JavaScript code inside the init() function
   let nums = [];
-  if (nums.includes(38, 38, 40, 40, 37, 39, 37, 39, 66, 65)){
-    alert('COngratulations murherfucka!')
+  while(nums.includes(38, 38, 40, 40, 37, 39, 37, 39, 66, 65) === false){
+    document.addEventListener('keydown', (event) => {
+      //alert(event.which);
+      nums.push(event.which);
+    })
   }
-  document.addEventListener('keydown', (event) => {
-    //alert(event.which);
-    nums.push(event.which);
-  })
-  console.log(nums)
-}
+  alert('Congrats!')
+} 
