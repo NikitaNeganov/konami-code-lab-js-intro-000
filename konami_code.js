a28f6821b5e95b28b0858eb2e5fb95739e4642bc
Nikita Neganov <nikitaneganov@icloud.com>
2017-10-17 09:00:14 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,13 +1,13 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const array = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
   // Write your JavaScript code inside the init() function
   let index = 0;
   document.addEventListener('keydown', onKeyDownHandler);
   function onKeyDownHandler(e){
     const key = parseInt(e.which);
-    if (key === code[index]){
+    if (key === array[index]){
       index++;
-      if (index === code.length){
+      if (index === array.length){
         alert('yuppee!');
         index = 0;
       }
