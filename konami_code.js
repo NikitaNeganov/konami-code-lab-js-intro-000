a90e86b832341e67645d3da47ae84f5d42f65bfb
Nikita Neganov <nikitaneganov@icloud.com>
2017-10-17 08:09:13 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,20 +2,17 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
-let index = 0;
-  function listen(){
-    document.addEventListener('keydown', (event)=>{
-      let key = parseInt(e.which);
-      return key;
+  let index = 0;
+  function check(){
+    document.addEventListener('keydown', (e)=>{
+      if (parseInt(e.which) === code[index]){
+        index++;
+      } else {
+        index = 0;
+      }
     })
   }
-  function increaseIndex(){
-    if (key = code[0]){
-      index++;
-    }
-  }
   while (index < 10){
-    listen();
-    increaseIndex();
-  } alert('yopta');
+    check();
+  }alert('yo');
 }
